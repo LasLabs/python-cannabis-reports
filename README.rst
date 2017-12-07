e381953e20d1c2ea6a7916f5fed7dbdd7083d5a6
Dave Lasley <dave@laslabs.com>
2017-12-07 09:52:02 -0800

    [ADD] Template files (Python + MIT) * Codeclimate * Coveragerc * Gitignore * Travis * License (MIT) * ReadMe * setup.py

---
+++ README.rst
@@ -0,0 +1,57 @@
+|License MIT| | |Build Status| | |Coveralls Status| | |Codecov Status| | |Code Climate|
+
+=======================
+Python Cannabis Reports
+=======================
+
+This library allows you to interact with the `Cannabis Reports API
+<https://developers.cannabisreports.com/docs>`_ using Python.
+
+Installation
+============
+
+*
+
+Setup
+=====
+
+* 
+
+Usage
+=====
+
+* `Read The API Documentation <https://laslabs.github.io/python-cannabis-reports>`_
+
+Known Issues / Road Map
+=======================
+
+-  Installation, setup, usage - in ReadMe
+
+Credits
+=======
+
+Contributors
+------------
+
+* Dave Lasley <dave@laslabs.com>
+
+Maintainer
+----------
+
+.. image:: https://laslabs.com/logo.png
+   :alt: LasLabs Inc.
+   :target: https://laslabs.com
+
+This module is maintained by LasLabs Inc.
+
+.. |Build Status| image:: https://api.travis-ci.org/LasLabs/python-cannabis-reports.svg?branch=master
+   :target: https://travis-ci.org/LasLabs/python-cannabis-reports
+.. |Coveralls Status| image:: https://coveralls.io/repos/LasLabs/python-cannabis-reports/badge.svg?branch=master
+   :target: https://coveralls.io/r/LasLabs/python-cannabis-reports?branch=master
+.. |Codecov Status| image:: https://codecov.io/gh/LasLabs/python-cannabis-reports/branch/master/graph/badge.svg
+   :target: https://codecov.io/gh/LasLabs/python-cannabis-reports
+.. |Code Climate| image:: https://codeclimate.com/github/LasLabs/python-cannabis-reports/badges/gpa.svg
+   :target: https://codeclimate.com/github/LasLabs/python-cannabis-reports
+.. |License MIT| image:: https://img.shields.io/badge/license-MIT-blue.svg
+   :target: https://opensource.org/licenses/MIT
+   :alt: License: MIT
---
+++ setup.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-TODAY LasLabs Inc.
+# License MIT (https://opensource.org/licenses/MIT).
+
+from setuptools import Command, setup
+from setuptools import find_packages
+from unittest import TestLoader, TextTestRunner
+
+from os import path
+
+
+PROJECT = 'cannabis-reports'
+SHORT_DESC = 'This library allows you to interact with Cannabis Reports ' \
+             'using Python.'
+README_FILE = 'README.rst'
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+
+setup_vals = {
+    'name': PROJECT,
+    'author': 'LasLabs Inc.',
+    'author_email': 'support@laslabs.com',
+    'description': SHORT_DESC,
+    'url': 'https://laslabs.github.io/python-%s' % PROJECT,
+    'download_url': 'https://github.com/LasLabs/python-%s' % PROJECT,
+    'license': 'MIT',
+    'classifiers': CLASSIFIERS,
+}
+
+
+if path.exists(README_FILE):
+    with open(README_FILE) as fh:
+        setup_vals['long_description'] = fh.read()
+
+
+class FailTestException(Exception):
+    """ It provides a failing build """
+    pass
+
+
+class Tests(Command):
+    """ Run test & coverage, save reports as XML """
+
+    user_options = []  # < For Command API compatibility
+
+    def initialize_options(self, ):
+        pass
+
+    def finalize_options(self, ):
+        pass
+
+    def run(self, ):
+        loader = TestLoader()
+        tests = loader.discover('.', 'test_*.py')
+        t = TextTestRunner(verbosity=1)
+        res = t.run(tests)
+        if not res.wasSuccessful():
+            raise FailTestException()
+
+
+if __name__ == "__main__":
+    setup(
+        packages=find_packages(exclude='tests'),
+        use_scm_version=True,
+        cmdclass={'test': Tests},
+        install_requires=[
+            'properties',
+            'requests',
+        ],
+        setup_requires=[
+            'setuptools_scm',
+        ],
+        tests_require=[
+            'mock',
+        ],
+        **setup_vals
+    )
